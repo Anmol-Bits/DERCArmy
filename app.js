40da53d4445131eb968f44d7015ad9fec0a456b6
Anmol Sharma <as1005211@gmail.com>
2021-11-20 11:59:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-console.log('this is my js');
-
-let slide = document.getElementsByClassName("sliding-images");
-
-slide.addEventListener('hover', function(){
-    slide.style.animation = "";
-});
+$("table").hide();
+setTimeout(function () {
+    $("table").show();
+}, 10)
